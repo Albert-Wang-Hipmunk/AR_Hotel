52f668ba06029a70354e3ec1c04ffd4831428c77
Albert <albert.wang@hipmunk.com>
2018-11-07 20:12:04 -0800

    add multiple himpunk card

--- app/src/main/java/com/example/mooqoo/myapplication/AugmentedImageActivity.kt
+++ app/src/main/java/com/example/mooqoo/myapplication/AugmentedImageActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.example.mooqoo.myapplication.Node.AnimationNode
 import com.example.mooqoo.myapplication.Node.BugAnimationNode
@@ -11,6 +13,7 @@ import com.google.ar.core.*
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Quaternion
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.rendering.ViewRenderable
@@ -22,6 +25,8 @@ import java.util.HashMap
 
 class AugmentedImageActivity : AppCompatActivity() {
     lateinit var arFragment: ArFragment
+
+    private val hipmunkRenerableList: MutableList<ViewRenderable?> = MutableList(3) { _ -> null}
     private val augmentedImageMap: MutableMap<AugmentedImage, Node> = HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +35,28 @@ class AugmentedImageActivity : AppCompatActivity() {
 
         arFragment = augmented_image_fragment as ArFragment
         arFragment.arSceneView.scene.addOnUpdateListener { onUpdateFrame(it) }
+
+        // TODO
+        createHipmunkCard(0, "Hotel Drisco, SF ", 139.00, R.drawable.hotel_1)
+        createHipmunkCard(1, "Phoenix Hotel, SF", 79.00, R.drawable.hotel_2)
+        createHipmunkCard(2, "Hotel Beresford, SF", 124.00, R.drawable.hotel_3)
+    }
+
+
+    fun createHipmunkCard(index: Int, hotelName: String, hotelPrice: Double, hotelImgId: Int) {
+        ViewRenderable.builder()
+            .setView(this, R.layout.hipmunk_card)
+            .build()
+            .thenAccept {
+                hipmunkRenerableList[index] = it
+                it.view.apply {
+                    setOnClickListener { onHipmunkCardClicked() }
+                    findViewById<TextView>(R.id.tv_hotel_name).text = hotelName
+                    findViewById<TextView>(R.id.tv_hotel_price).text = "$$hotelPrice"
+                    findViewById<ImageView>(R.id.iv_hotel_src).setImageResource(hotelImgId)
+                }
+//                addNodeToScene(arFragment, augmentedImage, it)
+            }
     }
 
     fun onUpdateFrame(frameTime: FrameTime) {
@@ -43,17 +70,21 @@ class AugmentedImageActivity : AppCompatActivity() {
                     // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
                     // but not yet tracked.
                     val text = "Detected Image " + augmentedImage.index
+                    Log.d("TESTT", "TrackingState.PAUSED: $text")
 //                    SnackbarHelper.getInstance().showMessage(this, text)
                 }
 
                 TrackingState.TRACKING -> {
                     // Have to switch to UI Thread to update View.
 //                    fitToScanView.setVisibility(View.GONE)
-
+                    Log.d("TESTT", "TrackingState.TRACKING------------")
                     // Create a new anchor for newly found images.
                     if (!augmentedImageMap.containsKey(augmentedImage)) {
                         Toast.makeText(this, "hipmunk image found!", Toast.LENGTH_SHORT).show()
                         placeObject(augmentedImage)
+                    } else {
+//                        val node = augmentedImageMap.get(augmentedImage)
+//                        node!!.setLookDirection(node.worldPosition)
                     }
                 }
 
@@ -63,21 +94,37 @@ class AugmentedImageActivity : AppCompatActivity() {
     }
 
     private fun placeObject(augmentedImage: AugmentedImage) {
-        ViewRenderable.builder()
-                .setView(this, R.layout.hipmunk_card)
-                .build()
-                .thenAccept { addNodeToScene(arFragment, augmentedImage, it) }
+//        ViewRenderable.builder()
+//                .setView(this, R.layout.hipmunk_card)
+//                .build()
+//                .thenAccept {
+//                    it.view.setOnClickListener { onHipmunkCardClicked() }
+//                    addNodeToScene(arFragment, augmentedImage, it)
+//                }
+        addNodeToScene(arFragment, augmentedImage, hipmunkRenerableList[0])
     }
 
-    private fun addNodeToScene(fragment: ArFragment, augmentedImage: AugmentedImage, renderable: Renderable) {
-        val anchorNode = AnchorNode(augmentedImage.createAnchor(augmentedImage.centerPose))
-        val node = BugAnimationNode()// AnimationNode(fragment.transformationSystem)
-        node.renderable = renderable
-        node.worldScale = Vector3(0.3f, 0.3f, 0.3f)
-//        node.localScale = Vector3(0.05f, 0.05f, 0.05f)
-        node.setParent(anchorNode)
-        node.animateRotate()
+    private fun onHipmunkCardClicked() {
+        Toast.makeText(this, "hipmunk card is clicked!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, augmentedImage: AugmentedImage, renderable: Renderable?) {
+        if (renderable == null) {
+            Toast.makeText(this, "renderable is null", Toast.LENGTH_SHORT).show()
+            return
+        }
+        var pos = augmentedImage.centerPose
+        var rotArray = pos.rotationQuaternion
+        rotArray[0] = 0.0f
+        rotArray[2] = 0.0f
+        val newPos = Pose(pos.translation, rotArray)
+
+        val anchorNode = AnchorNode(augmentedImage.createAnchor(newPos))
         fragment.arSceneView.scene.addChild(anchorNode)
+        val node = BugAnimationNode()// AnimationNode(fragment.transformationSystem)
+        node.setParent(anchorNode)
+        node.renderable = renderable
+        node.worldScale = Vector3(0.2f, 0.2f, 0.2f)
         augmentedImageMap.put(augmentedImage, node)
 //        node.select()
     }
